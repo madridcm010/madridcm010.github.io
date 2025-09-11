708a26c972773551961ee6d34481badb25c4dee0
madridcm010 <snowfallslower@gmail.com>
2025-09-10 23:31:19 -0400

    final touches

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [DatePipe, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+today: Date = new Date();
 
 }
